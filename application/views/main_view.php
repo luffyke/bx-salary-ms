bd44f583b31c7a6a878e572d62391e0fe6e1afbf
kxt <luffyke@gmail.com>
2012-04-11 23:02:57 +0800

    main page view and css

---
+++ application/views/main_view.php
@@ -0,0 +1,75 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+<meta charset="UTF-8">
+<link rel="stylesheet" type="text/css" href="css/main.css">
+<title>bx-salary</title>
+</head>
+<body>
+	<div class="main_box">
+		<div class="top_area_box">
+			<div class="top_area_logo"><a href="#" >返回首页</a></div>
+			<div class="top_area_title">Welcome to bx-salary [beyondx-享受编程的乐趣]</div>
+			<div class="top_area_logout">当前管理员:kxt [ <a href="#">退出</a> ]</div>
+			<div class="clear"></div>
+		</div>
+		<div class="content_box">
+			<div class="left_menu_box">
+				<div class="left_menu">
+					<ul>
+						<li class="caption">
+							<strong>个人信息管理</strong>
+							<ol>
+								<li>
+									<a href="#">个人资料</a>
+								</li>
+								<li>
+									<a href="#">修改密码</a>
+								</li>
+							</ol>
+						</li>
+					</ul>
+					<ul>
+						<li class="caption">
+							<strong>工资信息管理</strong>
+							<ol>
+								<li>
+									<a href="#">我的工资</a>
+								</li>
+								<li>
+									<a href="#">录入工资</a>
+								</li>
+							</ol>
+						</li>
+					</ul>
+					<ul>
+						<li class="caption">
+							<strong>工作经历管理</strong>
+							<ol>
+								<li>
+									<a href="#">工作经历</a>
+								</li>
+							</ol>
+						</li>
+					</ul>
+					<ul>
+						<li class="caption">
+							<strong>公积金保险管理</strong>
+							<ol>
+								<li>
+									<a href="#">公积金管理</a>
+								</li>
+								<li>
+									<a href="#">保险管理</a>
+								</li>
+							</ol>
+						</li>
+					</ul>
+				</div>
+			</div>
+		</div>
+		<div class="clear"></div>
+		<div class="footer_box"></div>
+	</div>
+</body>
+</html>
