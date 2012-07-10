755ceaf03849cb2b6ebcae5a0c27af330587e927
kxt <luffyke@gmail.com>
2012-07-10 22:13:11 +0800

    bug fix - miss checking /uiser/change_password

--- application/hooks/acl.php
+++ application/hooks/acl.php
@@ -3,18 +3,20 @@ class Acl {
 
 	private $CI;
 
-	public function __construct(){
+	public function __construct() {
 		$this->CI = & get_instance();
 	}
 
-	public function auth(){
-		if (!preg_match("/user.*/i", uri_string())) {
+	public function auth() {
+		if (!preg_match('/^(?!user\/change_password)user.*$/', uri_string())) {
 			$user_id = $this->CI->session->userdata('user_id');
 			if(empty($user_id)) {
-				redirect('/user/login');
+				redirect('/user');
 				return;
 			}
 		}
 	}
 }
-?>
+
+/* End of file acl.php */
+/* Location: ./application/hooks/Acl.php */
