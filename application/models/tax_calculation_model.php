25b09f498d62d80b0eccf936e36b5037358b2644
kxt <luffyke@gmail.com>
2013-03-28 18:05:27 +0800

    Update tax_calculation_model.php

--- application/models/tax_calculation_model.php
+++ application/models/tax_calculation_model.php
@@ -17,6 +17,47 @@ class Tax_calculation_model extends CI_Model {
 
 	function __construct() {
 		parent::__construct();
+		$this->load->database();
+	}
+	
+	function get_by_id($id) {
+		$result = $this->db->get_where($this->table_name, array($this->id=>$id));
+		return $result;
+	}
+	
+	function get_by_sequence($sequence) {
+		$result = $this->db->get_where($this->table_name, array($this->sequence=>$sequence));
+		return $result;
+	}
+	
+	function insert_tax_calculation($sequence, $from_amount, $to_amount, $rate, $deducting_amount) {
+		$tax_calculation_data = array($this->sequence=>$sequence, $this->from_amount=>$from_amount, $this->to_amount=>$to_amount, $this->rate=>$rate, $this->deducting_amount=>$deducting_amount);
+		$this->db->insert($this->table_name, $tax_calculation_data);
+		if ($this->db->affected_rows() == 1) {
+			return TRUE;
+		} else {
+			return FALSE;
+		}
+	}
+	
+	function update_tax_calculation($id, $sequence, $from_amount, $to_amount, $rate, $deducting_amount) {
+		$tax_calculation_data = array($this->sequence=>$sequence, $this->from_amount=>$from_amount, $this->to_amount=>$to_amount, $this->rate=>$rate, $this->deducting_amount=>$deducting_amount);
+		$this->db->where($this->id, $id);
+		$this->db->update($this->table_name, $tax_calculation_data);
+		if ($this->db->affected_rows() == 1) {
+			return TRUE;
+		} else {
+			return FALSE;
+		}
+	}
+	
+	function delete_tax_calculation($id) {
+		$this->db->delete($this->table_name, array($this->id=>$id));
+		if ($this->db->affected_rows() == 1) {
+			return TRUE;
+		} else {
+			return FALSE;
+		}
 	}
 	
 }
