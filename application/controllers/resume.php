04fa33528906ac470e426922bca528f41b2b03f3
kxt <luffyke@gmail.com>
2013-06-28 00:39:49 +0800

    Add resume function

---
+++ application/controllers/resume.php
@@ -0,0 +1,11 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class Resume extends CI_Controller {
+
+	public function index() {
+		$this->load->view('resume_view');
+	}
+}
+
+/* End of file resume.php */
+/* Location: ./application/controllers/resume.php */
