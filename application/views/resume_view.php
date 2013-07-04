29b84e0f0e1ef22065be66665505abf0e0a27c07
kxt <luffyke@gmail.com>
2013-07-04 15:32:14 +0800

    Resume structure

--- application/views/resume_view.php
+++ application/views/resume_view.php
@@ -1,8 +1,44 @@
 <html>
 <head>
+<style>
+body {}
 
+.resume {}
+.header {}
+.basics {}
+.details {}
+
+.github {}
+.experiences {}
+.projects {}
+.skills {}
+.english {}
+.hobbies {}
+
+.thin_split_line {}
+.thick_split_line {}
+.link {}
+</style>
 </head>
 <body>
-This is resume
+  <div class="resume">
+		<div class="header"></div>
+		<div class="thin_split_line"></div>
+		<div class="basics"></div>
+		<div class="thick_split_line"></div>
+		<div class="details">
+			<div class="github"></div>
+			<div class="thin_split_line"></div>
+			<div class="experiences"></div>
+			<div class="thin_split_line"></div>
+			<div class="projects"></div>
+			<div class="thin_split_line"></div>
+			<div class="skills"></div>
+			<div class="thin_split_line"></div>
+			<div class="english"></div>
+			<div class="thin_split_line"></div>
+			<div class="hobbies"></div>
+		</div>
+	</div>
 </body>
 </html>
