9bfd0c5eef29f9737086ca3cb6124ff8a36a9b9d
kxt <luffyke@gmail.com>
2013-07-07 00:06:31 +0800

    Resume add basics and experiences

--- application/views/resume_view.php
+++ application/views/resume_view.php
@@ -1,8 +1,139 @@
-<html>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
 <head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+<style>
+body {
+	font-family: 'Consolas', 'Verdana', 'sans-serif', '微软雅黑';
+}
 
+.resume {
+	border : 1px solid #D9D9D9;
+	margin : 0 auto;
+	width : 800px;
+}
+.header {
+	font-size : 18px;
+	font-weight: bold;
+	text-align: center;
+	letter-spacing: 5px;
+	height: 40px;
+	line-height: 40px;
+}
+.basics {
+	font-size: 15px;
+}
+.row {
+	width : 80%;
+	height: 30px;
+	line-height: 30px;
+}
+.title {
+	display : inline-block;
+	border-right : 1px solid #D9D9D9;
+	border-bottom : 1px solid #D9D9D9;
+	width : 150px;
+	text-align: center;
+	font-weight: bold;
+	letter-spacing: 1px;
+}
+.content {
+	display : inline-block;
+	border-right : 1px solid #D9D9D9;
+	border-bottom : 1px solid #D9D9D9;
+	width : 480px;
+}
+.details {
+	font-size: 13px;
+}
+
+.details_title {
+	text-align : center;
+	font-weight : bold;
+	letter-spacing : 2px;
+}
+
+.details_content {
+	padding-left: 50px;
+}
+
+.td_width {
+	width: 6%;
+}
+
+.experiences {
+  	padding-top: 5px;
+  	padding-bottom: 5px;
+}
+.projects {}
+.skills {}
+.english {}
+.hobbies {}
+
+.thin_split_line {
+	border-bottom : 1px solid #D9D9D9;
+	font-size : 0px;
+}
+.thick_split_line {
+	border-bottom : 5px solid #D9D9D9;
+	font-size : 0px;
+}
+.link {}
+</style>
 </head>
 <body>
-This is resume
+	<div class="resume">
+		<div class="header">个人简历</div>
+		<div class="thin_split_line"></div>
+		<div class="basics">
+			<div class="row">
+				<span class="title">姓名</span>
+				<span class="content">柯锡汤</span>
+			</div>
+			<div class="row">
+				<span class="title">学校专业</span>
+				<span class="content">华南农业大学 - 2010届软件工程</span>
+			</div>
+			<div class="row">
+				<span class="title">邮箱</span>
+				<span class="content">luffyke@gmail.com</span>
+			</div>
+			<div class="row">
+				<span class="title">电话</span>
+				<span class="content">13760672114</span>
+			</div>
+			<div class="row">
+				<span class="title">Github</span>
+				<span class="content">https://github.com/luffyke</span>
+			</div>
+		</div>
+		<div class="thick_split_line"></div>
+		<div class="details">
+			<div class="experiences">
+				<div class="details_title">工作经验</div>
+				<div class="details_content">
+					<table>
+						<tr><td class="td_width">时间 : </td><td>2009.11 - 2010.05</td></tr>
+						<tr><td>单位 : </td><td>深圳爱瑞思软件有限公司</td></tr>
+						<tr><td>职位 : </td><td>Java软件工程师实习生</td></tr>
+						<tr><td>职责 : </td><td>负责ScholarMate(科研之友)系统的代码编写以及系统分析</td></tr>
+						<tr><td></td><td></td></tr>
+						<tr><td>时间 : </td><td>2010.05 至今</td></tr>
+						<tr><td>单位 : </td><td>汇丰软件开发(广东)有限公司</td></tr>
+						<tr><td>职位 : </td><td>高级Java软件工程师</td></tr>
+						<tr><td>职责 : </td><td>主要负责汇丰银行HUB(HSBC Universal Banking)借贷系统HLS(HUB Lending System)/SLS(Software house Lending System)的项目开发和技术支持</td></tr>
+					</table>
+				</div>
+			</div>
+			<div class="thin_split_line"></div>
+			<div class="projects"></div>
+			<div class="thin_split_line"></div>
+			<div class="skills"></div>
+			<div class="thin_split_line"></div>
+			<div class="english"></div>
+			<div class="thin_split_line"></div>
+			<div class="hobbies"></div>
+		</div>
+	</div>
 </body>
 </html>
